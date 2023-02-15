8e6fde1ca844438e65d983dec0d6f4fa89950754
Nathan Downer <nathandowner123@gmail.com>
2023-02-15 10:28:08 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex h-screen flex-col items-center justify-center bg-gray-200">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
